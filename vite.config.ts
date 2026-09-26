649517b73050ef3a9d37d961aa91a92f8ef2ffb2
Alexey Filonchikov <falex@au.ru>
2026-09-26 13:05:55 +0800

    ci: deploy to GitHub Pages via GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from 'node:url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/green-api-test/', // замени на реальное имя репо
   plugins: [react()],
   resolve: {
     alias: {
